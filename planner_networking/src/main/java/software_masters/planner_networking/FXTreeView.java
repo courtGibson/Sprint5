5d8f07357f8656f47883bce371ddae8b14651f82
Courtney Gibson <courtney.gibson@centre.edu>
2019-04-09 23:58:55 -0400

    edit

--- planner_networking/src/main/java/software_masters/planner_networking/FXTreeView.java
+++ planner_networking/src/main/java/software_masters/planner_networking/FXTreeView.java
@@ -1,6 +1,11 @@
 package software_masters.planner_networking;
 
+import java.rmi.AccessException;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 
 import javax.swing.event.ChangeListener;
@@ -12,43 +17,113 @@ import javafx.event.EventHandler;
 import javafx.event.EventType;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
+import javafx.stage.Window;
+import javafx.stage.WindowEvent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
 public class FXTreeView extends Application
 {
 	VBox root;
-	TextField textFieldTop;
+	Text textTop;
 	TextField textFieldCenter;
+	Node planRoot;
+	Node currNode;
+	PlanFile planFile;
+
+	BorderPane bp;
+	Button btn1;
+	Button btn2;
+	Button btn3;
 	
+	static Server testServer;
+	static Client testClient;
+	static Server actualServer;
+	static Registry registry;
 
 	public static void main(String[] args)
 	{
 		Application.launch(args);
 	}
+	
+	private void startUp() throws IllegalArgumentException, RemoteException
+	{
+	
+		
+		Plan centrePlan = new Centre();
+		//this.planRoot = centrePlan.getRoot();
+		centrePlan.setName("Centre 2019");
+		centrePlan.addNode(centrePlan.getRoot().getChildren().get(0).getChildren().get(0));
+		centrePlan.getRoot().setData("This is the data of the root where we are hoping something will show up...");
+		
+		
+		try
+		{
+			registry = LocateRegistry.createRegistry(1078);
+			ServerImplementation server = new ServerImplementation();
+			actualServer = server;
+			Server stub = (Server) UnicastRemoteObject.exportObject(server, 0);
+			registry.rebind("PlannerServer", stub);
+			testServer = (Server) registry.lookup("PlannerServer");
+			testClient = new Client(testServer);
+			
+			this.planFile = new PlanFile(null, false, centrePlan);
+			planFile.setYear("2019");
+			
+			testClient.login("admin", "admin");
+			actualServer.addDepartment("Centre", "0");
+			actualServer.addPlanTemplate("Centre 2019", planFile);
+			//testClient.login("user", "user");
+			
+			
+			
+			
+			
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		
+		
+		testClient.getPlan("2019");
+		
+		this.planRoot = testClient.getCurrNode();
+		
+		
+	}
 
 	@Override
 	public void start(Stage primaryStage) throws RemoteException
 	{
+		primaryStage.close();
+		startUp();
+		
 		// Makes the treeview and returns a VBox
+		
 		VBox treeRoot = makeTree();
 
 		primaryStage.setTitle("Plan View");
 
 		// Making a border pane
-		BorderPane bp = new BorderPane();
+		this.bp = new BorderPane();
 
 		// Create the Text Nodes
 		Text topText = new Text("Plan View");
-
+		BorderPane.setAlignment(topText, Pos.CENTER);
+		
 		Text leftText = new Text("This is where a tree should be");
+		BorderPane.setAlignment(leftText, Pos.CENTER);
 		BorderPane bpCenter = new BorderPane();
 
 		bp.setCenter(bpCenter);
@@ -56,6 +131,8 @@ public class FXTreeView extends Application
 		bp.setLeft(leftText);
 
 		Text topTextBPCenter = new Text("Strategy 1");
+		
+		//BorderPane.setAlignment(topTextBP, Pos.CENTER);
 		bpCenter.setTop(topTextBPCenter);
 
 		makeRightSide(bp);
@@ -63,13 +140,17 @@ public class FXTreeView extends Application
 		bp.setLeft(treeRoot);
 
 		// Making the text field in the middle in bpCenter
-		this.textFieldTop = new TextField();
-		textFieldTop.setPromptText("Node Name");
-		bpCenter.setTop(textFieldTop);
+		this.textTop = new Text();
+		textTop.setText("Node Name");
+		bpCenter.setTop(textTop);
+	    
+		
 
 		this.textFieldCenter = new TextField();
 		textFieldCenter.setPromptText("Node Contents");
 		bpCenter.setCenter(textFieldCenter);
+		
+		
 
 		// Set the Size of the BP
 		bp.setPrefSize(1000, 600);
@@ -81,36 +162,149 @@ public class FXTreeView extends Application
 		primaryStage.setTitle("Team Sport");
 		// Display the Stage
 		primaryStage.show();
+		
+		primaryStage.setOnCloseRequest( event -> {try
+		{
+			stop();
+		} catch (RemoteException | NotBoundException e)
+		{
+			
+			e.printStackTrace();
+		}} );
 
+	}
+	
+	@Override
+	public void stop() throws AccessException, RemoteException, NotBoundException
+	{
+		registry.unbind("PlannerServer");
+		// Unexport; this will also remove us from the RMI runtime
+		UnicastRemoteObject.unexportObject(registry, true);
+		System.out.println("Closing RMI Server");
+	}
+	
+	
+	
+	private void removeNode(Button btn1) throws RemoteException
+	{
+		this.testClient.setCurrNode(this.currNode);
+		this.testClient.removeBranch();
+		
+		VBox newTree = makeTree();
+		this.bp.setLeft(newTree);
+		this.textFieldCenter.clear();
+		this.textTop.setText("");
+		this.currNode = null;
+		
+		this.btn1.setDisable(true);
+		this.btn3.setDisable(false);
+		
+	}
+	
+	private void addNode(Button btn2) throws RemoteException
+	{
+		this.testClient.setCurrNode(this.currNode);
+		this.testClient.addBranch();
+		
+		VBox newTree = makeTree();
+		this.bp.setLeft(newTree);
+		this.textFieldCenter.clear();
+		this.textTop.setText("");
+		this.currNode = null;
+		
+		this.btn2.setDisable(true);
+		this.btn3.setDisable(false);
+	}
+	
+	private void save(Button btn3) throws RemoteException
+	{
+		this.testClient.setCurrNode(this.currNode);
+		this.testClient.pushPlan(planFile);
+		
+		VBox newTree = makeTree();
+		this.bp.setLeft(newTree);
+		this.textFieldCenter.clear();
+		this.textTop.setText("");
+		this.currNode = null;
+		
+		this.btn2.setDisable(true);
 	}
 
 	private void makeRightSide(BorderPane bp)
 	{
 		// Building the buttons on the right
-		VBox rightSide = new VBox();
+		if (planFile.isCanEdit())
+		{
+			VBox rightSide = new VBox();
 
-		Text rightSideLabel = new Text("Editing Tools");
-		rightSide.getChildren().add(rightSideLabel);
+			Text rightSideLabel = new Text("Editing Tools");
+			rightSide.getChildren().add(rightSideLabel);
 
-		Button btn1 = new Button("Remove");
+			this.btn1 = new Button("Remove");
+			this.btn1.setDisable(true);
+			this.btn1.setOnAction(new EventHandler<ActionEvent>() {
+				@Override public void handle(ActionEvent e) {
+			        try
+					{
+						removeNode(btn1);
+					} catch (RemoteException e1)
+					{
+				
+						e1.printStackTrace();
+					}
+			    }
 
-		Button btn2 = new Button("Add Subsection");
 
-		Button btn3 = new Button("Save");
+			});
+			
 
-		rightSide.getChildren().addAll(btn1, btn2, btn3);
 
-		bp.setRight(rightSide);
+			this.btn2 = new Button("Add Subsection");
+			this.btn2.setDisable(true);
+			this.btn2.setOnAction(new EventHandler<ActionEvent>() {
+				@Override public void handle(ActionEvent e) {
+			        try
+					{
+						addNode(btn2);
+					} catch (RemoteException e1)
+					{
+			
+						e1.printStackTrace();
+					}
+			    }
+
+
+			});
+
+			this.btn3 = new Button("Save");
+			this.btn3.setDisable(true);
+			
+			this.btn3.setOnAction(new EventHandler<ActionEvent>() {
+				@Override public void handle(ActionEvent e) {
+			        try
+					{
+						save(btn3);
+					} catch (RemoteException e1)
+					{
+			
+						e1.printStackTrace();
+					}
+			    }
+
+
+			});
+
+			rightSide.getChildren().addAll(btn1, btn2, btn3);
+
+			bp.setRight(rightSide);
+		}
+	
 	}
 
 	private VBox makeTree() throws RemoteException
 	{
-
-		Plan centrePlan = new Centre();
-		centrePlan.setName("Centre 2019");
-		centrePlan.addNode(centrePlan.getRoot().getChildren().get(0).getChildren().get(0));
-		centrePlan.getRoot().setData("This is the data of the root where we are hoping something will show up...");
-		Node planRoot = centrePlan.getRoot();
+		
+		
 
 		// Create the TreeViewHelper
 //        TreeViewHelper helper = new TreeViewHelper();
@@ -118,22 +312,39 @@ public class FXTreeView extends Application
 //
 //        ArrayList<TreeItem> products = helper.getProducts(planRoot);
 
-		ArrayList<TreeItem> products = getProducts(planRoot);
+		TreeItem<Node> rootItem = getProducts(this.planRoot);
 
 		// Create the TreeView
-		TreeView treeView = new TreeView();
+		TreeView<Node> treeView = new TreeView<Node>();
+		treeView.setRoot(rootItem);
+		
+		treeView.getSelectionModel().selectedItemProperty()
+        .addListener((observable, oldValue, newValue) -> handleTreeClick(newValue));
 		// Create the Root TreeItem
 		
-		TreeItem rootItem = new TreeItem(centrePlan.getName());
+		//TreeItem<Node> rootItem = new TreeItem<Node>(centrePlan.getRoot());
 
 		// Add children to the root
-		rootItem.getChildren().addAll(products);
+		//rootItem.getChildren().addAll(products);
 		// Set the Root Node
-		treeView.setRoot(rootItem);
 
 		// Create the VBox
+		
 		VBox treeRoot = new VBox();
+		
+		
 		this.root = treeRoot;
+		
+		
+        Text planNameText = new Text();
+		
+		planNameText.setText(testClient.getCurrPlanFile().getPlan().getName());
+		treeRoot.setAlignment(Pos.CENTER);
+		
+		treeRoot.getChildren().add(planNameText);
+		//VBox.setAlignment(planNameText, Pos.CENTER);
+		
+	
 		// Add the TreeView to the VBox
 		treeRoot.getChildren().add(treeView);
 
@@ -142,23 +353,23 @@ public class FXTreeView extends Application
 	}
 
 	// This method creates an ArrayList of TreeItems (Products)
-	public ArrayList<TreeItem> getProducts(Node root) throws RemoteException
+	public TreeItem<Node> getProducts(Node root) throws RemoteException
 	{
 
 		// This will be the final ArrayList passed back to FXTreeView.java for build
 		// (should only hold Mission for centre)
-		ArrayList<TreeItem> FinalNodeList = new ArrayList<TreeItem>();
+		//ArrayList<TreeItem<Node>> FinalNodeList = new ArrayList<TreeItem<Node>>();
 
-		TreeItem currentTreeItem = new TreeItem(root.getName());
+		TreeItem<Node> currentTreeItem = new TreeItem<Node>(root);
 
-		FinalNodeList.add(currentTreeItem);
+		//FinalNodeList.add(currentTreeItem);
 
 		getKids(root, currentTreeItem);
 
-		return FinalNodeList;
+		return currentTreeItem;
 	}
 
-	private void getKids(Node parentNode, TreeItem parentTreeItem)
+	private void getKids(Node parentNode, TreeItem<Node> parentTreeItem)
 	{
 
 		if (parentNode.getChildren().isEmpty())
@@ -168,23 +379,7 @@ public class FXTreeView extends Application
 
 		for (int i = 0; i < parentNode.getChildren().size(); i++)
 		{
-			TreeItem newChild = new TreeItem(parentNode.getChildren().get(i));
-			
-			Node nodeChild = parentNode.getChildren().get(i);
-			
-			EventType MouseEvent = null;
-			nodeChild.addEventHandler(MouseEvent.MOUSE_PRESSED,
-					(event) -> editTextField(nodeChild));
-			
-			nodeChild.addEventHandler(MouseEvent.MOUSE_ENTERED, 
-				    new EventHandler<MouseEvent>() {
-				        @Override public void handle(MouseEvent e) {
-				            editTextField(nodeChild);
-				  
-				        }
-				});
-			
-// System.out.println(newValue.toString()));
+			TreeItem<Node> newChild = new TreeItem<Node>(parentNode.getChildren().get(i));
 
 			parentTreeItem.getChildren().add(newChild);
 			getKids(parentNode.getChildren().get(i), newChild);
@@ -193,10 +388,72 @@ public class FXTreeView extends Application
 
 	}
 	
-	private void editTextField(Node newValue)
+	private void handleTreeClick(TreeItem<Node> newValue)
 	{
-		this.textFieldTop.setText(newValue.toString());
-		this.textFieldCenter.setText( ((Node) newValue).getData());
+		this.textTop.setText(newValue.getValue().getName());
+		this.textFieldCenter.setText(newValue.getValue().getData());
+		
+		
+		/*textField.setOnKeyPressed(new EventHandler<KeyEvent>() 
+		{
+            public void handle(KeyEvent ke) 
+            {
+                System.out.println(textField.getText()+ke.getText());//.add("Key Pressed: " + ke.getText()););
+                currNode.setName(textField.getText()+ke.getText());
+            }
+        });*/
+		
+		textFieldCenter.setOnKeyPressed(new EventHandler<KeyEvent>() 
+		{
+            public void handle(KeyEvent ke) 
+            {
+                System.out.println(textFieldCenter.getText()+ke.getText());//.add("Key Pressed: " + ke.getText()););
+                currNode.setData(textFieldCenter.getText()+ke.getText());
+            }
+        });
+		
+		/*textField.textProperty().addListener((obs, oldText, newText) -> {
+		    System.out.println("Text changed from "+oldText+" to "+newText);
+		    this.btn3.setDisable(false);
+		    
+		    String newData = textField.getText();
+		    this.currNode.setName(newData);
+		    // ...
+		});
+		
+		textFieldCenter.textProperty().addListener((obs, oldText, newText) -> {
+			System.out.println("Text changed from "+oldText+" to "+newText);
+			this.btn3.setDisable(false);
+			
+		    String newData = textFieldCenter.getText();
+		    this.currNode.setData(newData);
+		    // ...
+		});*/
+		
+		this.currNode = newValue.getValue();
+		
+		this.btn1.setDisable(false);
+		this.btn2.setDisable(false);
+		this.btn3.setDisable(false);
+
+		
+	}
+	
+	/*public void setServer(Server server)
+	{
+		this.testServer = server;
+	}*/
+	public Server getServer()
+	{
+		return this.testServer;
+	}
+	/*public void setClient(Client client)
+	{
+		this.testClient = client;
+	}*/
+	public Client getClient() 
+	{
+		return this.testClient;
 	}
 
 }
