49583f0b09622bdcd3910134cdf653d7c61f38b5
Courtney Gibson <courtney.gibson@centre.edu>
2019-04-15 22:36:54 -0400

    edit plan view

--- planner_networking/src/main/java/fx/planView/PlanViewController.java
+++ planner_networking/src/main/java/fx/planView/PlanViewController.java
@@ -1,6 +1,57 @@
 package fx.planView;
 
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import software_masters.planner_networking.Client;
+
 public class PlanViewController
 {
+	Stage primaryStage;
+	BorderPane mainView;
+	Client testClient;
+	
+	
+	public Client getTestClient()
+	{
+	
+		return testClient;
+	}
+
+	public void setTestClient(Client testClient)
+	{
+	
+		this.testClient = testClient;
+	}
+
+	public Stage getPrimaryStage()
+	{
+	
+		return primaryStage;
+	}
+
+	public void setPrimaryStage(Stage primaryStage)
+	{
+	
+		this.primaryStage = primaryStage;
+	}
+
+	public BorderPane getMainView()
+	{
+	
+		return mainView;
+	}
+
+	public void setMainView(BorderPane mainView)
+	{
+	
+		this.mainView = mainView;
+	}
+	
+	Button saveBtn;
+	Button removeBtn;
+	Button addBtn;
+	
+	
 
 }
