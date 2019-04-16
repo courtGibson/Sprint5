cf748cd454c1236b02367200f769dea79965f752
Keenan Jones <elic.jones@centre.edu>
2019-04-15 21:23:22 -0400

    Work on Server View

--- planner_networking/src/main/java/serverView/ServerViewController.java
+++ planner_networking/src/main/java/serverView/ServerViewController.java
@@ -1,9 +1,16 @@
 package serverView;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import software_masters.planner_networking.Client;
+import software_masters.planner_networking.Server;
+import software_masters.planner_networking.ServerImplementation;
 
 public class ServerViewController
 {
@@ -19,9 +26,45 @@ public class ServerViewController
 	@FXML
 	private Button ServerSubmitButton;
 	
+	static Server testServer;
+	static Client testClient;
+	static Server actualServer;
+	static Registry registry;
 	
-	public void help() {
-		System.out.println("Help");
+	public void connectToServer() {
+		if(DefaultServerButton.isSelected()) {
+			try
+			{
+				registry = LocateRegistry.createRegistry(1075);
+				ServerImplementation server = new ServerImplementation();
+				actualServer = server;
+				Server stub = (Server) UnicastRemoteObject.exportObject(server, 0);
+				registry.rebind("PlannerServer", stub);
+				testServer = (Server) registry.lookup("PlannerServer");
+				testClient = new Client(testServer);
+				
+			} catch (Exception e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		else {
+			
+			String hostName = OtherServerTextField.getAccessibleText();
+			try
+			{
+				registry = LocateRegistry.getRegistry(hostName, 1075);
+				testServer = (Server) registry.lookup("PlannerServer");
+				testClient = new Client(testServer);
+				
+			} catch (Exception e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		
 	}
 }
