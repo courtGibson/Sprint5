e670610e93f695a04a60b70265e1508ae5a53a7a
pharies <pharies@10.84.16.39>
2019-04-15 21:04:41 -0400

    login view controller

--- planner_networking/src/main/java/loginView/LoginViewController.java
+++ planner_networking/src/main/java/loginView/LoginViewController.java
@@ -1,12 +1,18 @@
 package loginView;
 
+import java.rmi.RemoteException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import software_masters.planner_networking.Client;
+
+
 
 public class LoginViewController
 {
+	public Client client;
 	@FXML
 	private TextField UsernameTextField;
 	
@@ -17,12 +23,14 @@ public class LoginViewController
 	private Button LoginSubmitButton;
 	
 	@FXML
-	public void onButtonSubmit(ActionEvent event){
+	public void onButtonSubmit(ActionEvent event) throws IllegalArgumentException, RemoteException{
 		String username = UsernameTextField.textProperty().get();
 		String password = PasswordTextField.textProperty().get();
 		
 		System.out.println(username);
 		System.out.println(password);
+		
+		client.login(username, password);
 		
 	}
 	
