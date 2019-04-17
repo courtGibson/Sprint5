9c6a8702b685159f509f1d76a4961fab837f78c1
pharies <pharies@10.84.16.39>
2019-04-16 20:48:57 -0400

    made cleint inside choose

--- planner_networking/src/main/java/fx/choosePlan/ChoosePlanController.java
+++ planner_networking/src/main/java/fx/choosePlan/ChoosePlanController.java
@@ -35,6 +35,7 @@ import software_masters.planner_networking.ServerImplementation;
 public class ChoosePlanController
 {
 	public Plan plan;
+	public Client client;
 	
 	public void setTestClient(Client testClient)
 	{
