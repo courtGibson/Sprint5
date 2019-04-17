4442a78a1fcd73fe6bcfca2c34cfdfca938d099c
pharies <pharies@10.84.16.39>
2019-04-16 21:30:06 -0400

    need submit button

--- planner_networking/src/main/java/fx/planView/PlanViewController.java
+++ planner_networking/src/main/java/fx/planView/PlanViewController.java
@@ -4,14 +4,32 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import software_masters.planner_networking.Client;
+import software_masters.planner_networking.PlanFile;
 
 public class PlanViewController
 {
 	Stage primaryStage;
 	BorderPane mainView;
 	Client testClient;
+	PlanFile plan;
 	
 	
+	/**
+	 * @return the plan
+	 */
+	public PlanFile getPlan()
+	{
+		return plan;
+	}
+
+	/**
+	 * @param plan the plan to set
+	 */
+	public void setPlan(PlanFile plan)
+	{
+		this.plan = plan;
+	}
+
 	public Client getTestClient()
 	{
 	
