62f2e9fc40b671a7783687bf73f53f18045ab9b8
Courtney Gibson <courtney.gibson@centre.edu>
2019-04-19 08:27:01 -0400

    edit

--- planner_networking/src/main/java/fx/test/ServerSelectionTest.java
+++ planner_networking/src/main/java/fx/test/ServerSelectionTest.java
@@ -1,16 +1,21 @@
 package fx.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.testfx.assertions.api.Assertions.assertThat;
 
 import java.io.IOException;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import serverView.ServerViewController;
 import software_masters.planner_networking.Main;
@@ -55,13 +60,36 @@ public class ServerSelectionTest extends ApplicationTest
 			e.printStackTrace();
 		}
 		
+		
+		
 			
 	}
 	
 	
-	
 	//other methods
+	public void checkRBText(String id, String val)
+	{
+		assertThat(lookup(id).queryAs(Text.class)).hasText(val);
+	
+	}
+	
+
+	
+	
 	
     //@test methods
+	@Test
+	public void checkText()
+	{
+		
+		clickOn("#OtherServerButton");
+		sleep(1000);
+		clickOn("#DefaultServerButton");
+		sleep(1000);
+		
+		checkRBText("#localText", "Default: Local Host");
+		
+
+	}
 	
 }
