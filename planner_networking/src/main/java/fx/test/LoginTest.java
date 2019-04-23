c7c10c0eee6727a7d3ac679887f25cd0c70eebef
pharies <pharies@10.84.16.39>
2019-04-22 22:32:46 -0400

    h

--- planner_networking/src/main/java/fx/test/LoginTest.java
+++ planner_networking/src/main/java/fx/test/LoginTest.java
@@ -9,10 +9,10 @@ import org.junit.Test;
 import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
 
+
+
 import java.io.IOException;
-import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -27,107 +27,78 @@ import serverView.ServerViewController;
 import software_masters.planner_networking.Client;
 import software_masters.planner_networking.Main;
 import software_masters.planner_networking.Server;
-import software_masters.planner_networking.ServerImplementation;
+
 
 public class LoginTest extends ApplicationTest
 {
-
+	
 	static Server testServer;
 	static Client testClient;
 	static Server actualServer;
 	static Registry registry;
-	private Stage primaryStage;
-	BorderPane mainView;
+	private Stage stage;
 	
-
-
 	@Override
-	public void start(Stage stage) throws Exception
+	public void start(Stage stage) throws Exception 
 	{
-
-		this.primaryStage = stage;
-
+		this.stage=stage;
+	
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(Main.class.getResource("/loginView/loginView.fxml"));
-		try
-		{
+		try {
 			Scene s = new Scene(loader.load());
 			LoginViewController cont = loader.getController();
-			cont.setPrimaryStage(primaryStage);
-			primaryStage.setScene(s);
-			primaryStage.show();
+			cont.setPrimaryStage(stage);
+			stage.setScene(s);
+			stage.show();
+		} catch (IOException e) {
 			
-		} catch (IOException e)
-		{
-
-			e.printStackTrace();
-		}
-	}
-	
-	@Before
-	public void setUp () throws Exception 
-	{
-		System.out.println("Starting Test");
-		try
-		{
-			registry = LocateRegistry.createRegistry(1077);
-			
-			ServerImplementation server = ServerImplementation.load();
-			
-			actualServer = server;
-			Server stub = (Server) UnicastRemoteObject.exportObject(server, 0);
-			registry.rebind("PlannerServer", stub);
-			
-			this.testServer = (Server) registry.lookup("PlannerServer");
-			this.testClient = new Client(testServer);
-			
-		} catch (Exception e)
-		{
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
+			
 	}
 	
-	@After
-	public void tearDown () throws Exception
-	{
-		registry.unbind("PlannerServer");
-		UnicastRemoteObject.unexportObject(registry, true);
-		System.out.println("Closing RMI Server");
-	}
+	  @Test
+	  public void testUsernameInput() {
+		  
+		  clickOn("#UsernameTextField");
+		  write("This is the username");
+		  
+		  assertEquals(getText("#UsernameTextField"), "This is the username");
 
-	@Test
-	public void testUsernameInput()
-	{
-
-		clickOn("#UsernameTextField");
-		write("This is the username");
-
-		assertEquals(getText("#UsernameTextField"), "This is the username");
-
-	}
-
-	@Test
-	public void testPasswordInput()
-	{
-
-		clickOn("#PasswordTextField");
-		write("This is the password");
-
-		assertEquals(getText("#PasswordTextField"), "This is the password");
-
-	}
-
-
-
-	@SuppressWarnings("unchecked")
-	String getText(String label)
-	{
-
-		TextField thisTextField = (TextField) lookup(label).query();
-		return thisTextField.textProperty().get();
-
-	}
-
+		  
+	  }
+	  
+	  @Test
+	  public void testPasswordInput() {
+		  
+		  clickOn("#PasswordTextField");
+		  write("This is the password");
+		  
+		  assertEquals(getText("#PasswordTextField"), "This is the password");
+		  
+	  }
+	  
+	  @Test
+	  public void testEnterButton() {
+		  clickOn("#UsernameTextField");
+		  write("user");
+		  clickOn("#PasswordTextField");
+		  write("user");
+		  clickOn("#LoginSubmitButton");
+		  
+		  System.out.println("Here");
+		  assertEquals(getText("#PasswordTextField"), "user");
+		  
+	  }
+	  
+	  @SuppressWarnings("unchecked")	  
+	  String getText(String label)
+	  {
+		  TextField thisTextField = (TextField) lookup(label).query();
+		  return thisTextField.textProperty().get();
+			
+	  }
+	
 }
